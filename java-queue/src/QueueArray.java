d4d6743adb86dc8263e7ab3b182faf8f168b8c4a
JSQdev <148073798+devJSQ@users.noreply.github.com>
2024-03-23 17:35:53 +0300

    Update QueueArray.java

--- java-queue/src/QueueArray.java
+++ java-queue/src/QueueArray.java
@@ -1,45 +1,51 @@
-public class Queue {
+public class queueArray {
+    int [] queue;
+    int front;
+    int rear;
+    int size;
 
-    // Attributes
-    Node front;
-    Node rear;
-
-    // Constructor
-    public Queue() {
-        this.front = null;
-        this.rear = null;
+    public queueArray(int size){
+        queue = new int[size];
+        this.size = size;
+        front = -1;
+        rear = -1;
     }
 
-    // enqueue
-    public void enqueue(int newData) {
-        Node newNode = new Node(newData);
-        if (rear != null){
-            rear.next = newNode;
-        }
-        rear = newNode;
-        if (front == null){
-            front = rear;
-        }
-
-    }
-    // dequeue
-    public void dequeue() {
-        if(front == null){
-            System.out.println("queue is empty");
+    public void enqueue(int item){
+        //if queue is full
+        if (rear == size -1) {
+            System.out.println("queue is full");
             return;
+        }
+        //if Q is empty
+        if (front == -1 && rear == -1) {
+            front = 0;
+            rear = 0;
+        }else{
+            rear++;
         } 
-        int data = front.data;
-        front = front.next;
-        System.out.println(data);
+        queue[rear] = item;
+    }
+    public void dequeue(){
+        if(front == -1 && rear == -1){
+            System.out.println("empty");
+
+        }else if(front == 0 && rear == 0){
+            rear = -1;
+            front = -1;
+        }else{
+            front++;
+        }
         
     }
-    // display
-    public void display() {
-        Node x = front;
-        while (x != null) {
-            System.out.println(x.data);
-            x = x.next; 
-        }
 
+    public void display() {
+        // temporary variable to print queue from front to rear
+        int temp = front;
+        for(int i = temp; i <= rear; i++) {
+
+            System.out.println(queue[i]);
+        }
     }
+    
 }
